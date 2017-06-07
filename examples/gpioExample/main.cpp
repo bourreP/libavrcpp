f111b3998ff44b12456eb945cc4c60aaf668afee
kayou <pierre.bourre@gmail.com>
2017-06-07 12:03:32 +0200

    Add gpio example

---
+++ examples/gpioExample/main.cpp
@@ -0,0 +1,29 @@
+// 8MHz oscillator
+#define	F_CPU 8000000UL
+
+#include "../../libavrcpp/gpio.hpp"
+#include <util/delay.h>
+
+int main ()
+{
+    //Pin 5 of port B in output mode
+    B5::output();
+    B6::output();
+    B0::input();
+    //Loop
+    while(true)
+    {
+        // ON
+        B5::high();
+        _delay_ms(500);
+
+        // OFF
+        B5::low();
+        _delay_ms(500);
+
+        if (B0::read() == 1)
+            B6::toggle();
+    }
+
+    return 0;
+}
